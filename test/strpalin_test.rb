4f8b1b8d4ef089c1c196b368e060144513889137
Thao Hoang <thaominhhoagnvn@outlook.com>
2019-11-08 09:40:35 +0700

    add basic test for palindrome

--- test/strpalin_test.rb
+++ test/strpalin_test.rb
@@ -1,11 +1,19 @@
 require "test_helper"
 
 class StrpalinTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Strpalin::VERSION
+  def test_non_palindrome
+    assert !"apple".palindrome?
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_literal_palindrome
+    assert "racecar".palindrome?
+  end
+
+  def test_mixed_case_palindrome
+    assert "RAceCar".palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+    skip
   end
 end
